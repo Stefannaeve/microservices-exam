22c479b57a90732ea19b9e85910a0b4d83cfafad
stefan <stefan.naeve@hotmail.com>
2024-09-03 18:19:33 +0200

    Initial commit

---
+++ src/test/java/microservices/exam/MicroservicesExamApplicationTests.java
@@ -0,0 +1,13 @@
+package microservices.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
