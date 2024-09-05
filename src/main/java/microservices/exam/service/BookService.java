d9444b8b8626c3ec5bebe1dba94c289864084f3c
Stefan <stefan.naeve@hotmail.com>
2024-09-05 11:29:25 +0200

    Add logging to service

--- src/main/java/microservices/exam/service/BookService.java
+++ src/main/java/microservices/exam/service/BookService.java
@@ -1,5 +1,6 @@
 package microservices.exam.service;
 
+import lombok.extern.slf4j.Slf4j;
 import microservices.exam.models.Book;
 import microservices.exam.repository.BookRepository;
 import microservices.exam.utils.ApiResponse;
@@ -12,6 +13,7 @@ import static microservices.exam.utils.ApiResponse.Failure;
 import static microservices.exam.utils.ApiResponse.Success;
 
 @Service
+@Slf4j
 public class BookService {
 
     BookRepository bookRepository;
@@ -25,9 +27,10 @@ public class BookService {
         try {
             listBooks = Optional.of(bookRepository.findAll());
         } catch (Exception exception){
-            System.out.println(exception.getMessage());
+            log.error(exception.getMessage());
             return Failure(exception.getMessage());
         }
+        log.info("Returning list of books");
         return Success(listBooks);
     }
 
@@ -36,11 +39,14 @@ public class BookService {
             Book bookFromDatabase = bookRepository.findBookByTitleAndAuthorAndPublishDate(book.getTitle(), book.getAuthor(), book.getPublishDate());
             if (bookFromDatabase == null){
                 Optional<Book> savedBook = Optional.of(bookRepository.save(book));
+                log.info("Added book to database");
                 return Success(savedBook);
             } else {
+                log.error("Tried adding already existing book");
                 return Failure("Book already exists");
             }
         } catch (Exception exception){
+            log.error(exception.getMessage());
             return Failure(exception.getMessage());
         }
     }
