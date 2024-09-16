a3fb31607e501fc5de36fbfb2dc1089ac050ae70
Stefan <stefan.naeve@hotmail.com>
2024-09-16 13:04:32 +0200

    Small frontend api change

--- Frontend/my-next-app/src/interfaces/ApiResponse.ts
+++ Frontend/my-next-app/src/interfaces/ApiResponse.ts
@@ -1,5 +1,26 @@
-export interface ApiResponse<T> {
-    isSuccess: boolean,
-    object?: T,
-    errorMessage?: string
+// export interface ApiResponse<T> {
+//     isSuccess: boolean,
+//     object?: T,
+//     errorMessage?: string
+// }
+
+interface Success<T> {
+    value: T;
+}
+
+interface Failure<T> {
+    value?: T;
+    errorMessage: string;
+}
+
+type ApiResponse<T> = Success<T> | Failure<T>;
+
+// Something called type guards to implement type checking
+
+function isSuccess<T>(response: ApiResponse<T>): response is Success<T> {
+    return 'value' in response && !('httpStatus' in response)
+}
+
+function isFailure<T>(response: ApiResponse<T>): response is Failure<T> {
+    return 'httpStatus' in response && 'errorMessage' in response;
 }
