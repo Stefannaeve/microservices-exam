b5028d31642707e1a8dd0ad2cfe0a82f3cc0e180
Stefan <stefan.naeve@hotmail.com>
2024-09-18 16:34:01 +0200

    Manager module talks with comment module

---
+++ Manager/src/main/java/microservices/manager/apiResponse/ApiResponse.java
@@ -0,0 +1,10 @@
+package microservices.manager.apiResponse;
+
+import org.springframework.http.HttpStatus;
+
+import java.util.Optional;
+
+public sealed interface ApiResponse<T> {
+    record Success<K>(Optional<K> value) implements ApiResponse<K> {}
+    record Failure<K>(Optional<K> value, HttpStatus status, String errorMessage) implements ApiResponse<K> {}
+}
