43ed91b7175e7dcadbcfaad817bedb0080513d2a
Stefan <114489609+Stefannaeve@users.noreply.github.com>
2024-09-24 13:24:44 +0200

    Manager (#38)
    * Add manager module
    
    * Manager module talks with comment module
    
    * Add manager module
    
    * Manager module talks with comment module
    
    * Remove application.properties
    
    * Manager to comment communication complete including nice error handling
    
    * Manager module talks with comment module
    
    * Add manager module
    
    * Manager module talks with comment module
    
    * Remove application.properties
    
    * Manager to comment communication complete including nice error handling
    
    * Manager module talks with comment module
    
    * Add manager module
    
    * Manager module talks with comment module
    
    * Remove application.properties
    
    * Add manager module
    
    * Manager module talks with comment module
    
    * Manager module talks with comment module
    
    * Remove application.properties
    
    * Added Book handling in the manager. Now gets and posts
    
    * Add more functionality to book in manager, add regex handling for exception handling

--- Manager/src/main/java/microservices/manager/ManagerControllers/BookController.java
+++ Manager/src/main/java/microservices/manager/ManagerControllers/BookController.java
@@ -1,4 +1,61 @@
 package microservices.manager.ManagerControllers;
 
+import lombok.extern.slf4j.Slf4j;
+import microservices.manager.ManagerClients.BookClient;
+import microservices.manager.apiResponse.ApiResponse;
+import microservices.manager.dtos.BookDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/manager/book")
 public class BookController {
+    BookClient bookClient;
+
+    public BookController(BookClient bookClient){
+        this.bookClient = bookClient;
+    }
+
+    @GetMapping("fetchAll")
+    public ResponseEntity<ApiResponse<List<BookDTO>>> fetchAllBooks(){
+        ApiResponse<List<BookDTO>> books = bookClient.externalGetAllBooks();
+
+        switch (books){
+            case ApiResponse.Success<List<BookDTO>> success -> {
+                if (success.value().isPresent()){
+                    log.info("Success, returning {} books from book service", success.value().get().size());
+                } else {
+                    log.info("Success");
+                }
+                return ResponseEntity.status(200).body(success);
+            }
+            case ApiResponse.Failure<List<BookDTO>> failure -> {
+                log.error(failure.errorMessage());
+                return ResponseEntity.status(failure.status()).body(failure);
+            }
+        }
+    }
+
+    @PostMapping("/saveBook")
+    public ResponseEntity<ApiResponse<BookDTO>> saveBook(@RequestBody BookDTO book){
+        ApiResponse<BookDTO> savedBook = bookClient.externalSaveBook(book);
+
+        switch (savedBook){
+            case ApiResponse.Success<BookDTO> success -> {
+                if (success.value().isPresent()){
+                    log.info("Success, saved book with id: {}", success.value().get().getId());
+                } else {
+                    log.info("Success");
+                }
+                return ResponseEntity.status(200).body(success);
+            }
+            case ApiResponse.Failure<BookDTO> failure -> {
+                log.error(failure.errorMessage());
+                return ResponseEntity.status(failure.status()).body(failure);
+            }
+        }
+
+    }
 }
