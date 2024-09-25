21a86bddbdb062eb4c8641542a8d47ebec0f74f5
mbJobb <71829138+mbJobb@users.noreply.github.com>
2024-09-25 02:51:55 +0200

    start of user module

---
+++ User/src/main/java/microservices/user/user/models/User.java
@@ -0,0 +1,31 @@
+package microservices.user.user.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(generator = "user_id_generator")
+    @SequenceGenerator(name = "user_id_generator", sequenceName =
+            "user_id_seq", initialValue = 1)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @OneToMany
+    private List<BookId> books;
+}
