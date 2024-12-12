6d57e3e8f82731af9d3b698d180999f2c101cdab
Henrik-Pham <HenrikPHPham@gmail.com>
2024-12-12 10:35:48 +0100

    small comment fixes

--- Book/src/main/java/microservices/exam/eventDriven/BookEvent.java
+++ Book/src/main/java/microservices/exam/eventDriven/BookEvent.java
@@ -20,7 +20,7 @@ public class BookEvent {
     public BookEvent() {
     }
 
-    // Constructor for delete events
+    // delete events
     public BookEvent(Long bookId, String eventType) {
         this.bookId = bookId;
         this.eventType = eventType;
@@ -31,7 +31,7 @@ public class BookEvent {
         this.bookContent = null;
     }
 
-    // Constructor for create and update events
+    // create and update events
     public BookEvent(Long bookId, String eventType, String title, String author, int pages, Date publishDate, String bookContent) {
         this.bookId = bookId;
         this.eventType = eventType;
