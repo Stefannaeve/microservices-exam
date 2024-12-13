549fa26417f9d3a8a8945e1074ec63cf002f8bae
Stefan <stefan.naeve@hotmail.com>
2024-12-13 18:44:27 +0100

    Make manager and book work with ResponseEntity instead of ApiResponse

--- Book/src/main/java/microservices/exam/service/BookService.java
+++ Book/src/main/java/microservices/exam/service/BookService.java
@@ -80,6 +80,23 @@ public class BookService {
         }
     }
 
+  public ApiResponse<Book> fetchById(long id) {
+        ApiResponseBuilder<Book> apiResponseBuilder = new ApiResponseBuilder<>();
+        try {
+            Optional<Book> book = bookRepository.findById(id);
+            if (book.isPresent()) {
+                log.info("Book found with id: {}", id);
+                return apiResponseBuilder.success(book.get());
+            } else {
+                log.warn("Book with id {} not found", id);
+                return apiResponseBuilder.failure(HttpStatus.NOT_FOUND, "Book not found");
+            }
+        } catch (Exception exception) {
+            log.error("Error fetching book by id {}: {}", id, exception.getMessage());
+            return apiResponseBuilder.failure(HttpStatus.INTERNAL_SERVER_ERROR, "Something went wrong...");
+        }
+    }
+
     public ApiResponse<Void> deleteBookById(Long bookId) {
         ApiResponseBuilder<Void> apiResponseBuilder = new ApiResponseBuilder<>();
         try {
