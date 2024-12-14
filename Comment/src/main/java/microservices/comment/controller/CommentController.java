5da8b79a40d0587079fbb87c67747ddf396fa248
Stefan <stefan.naeve@hotmail.com>
2024-12-14 12:52:07 +0100

    Update CommentController to ResponseEntity

--- Comment/src/main/java/microservices/comment/controller/CommentController.java
+++ Comment/src/main/java/microservices/comment/controller/CommentController.java
@@ -1,17 +1,15 @@
 
 package microservices.comment.controller;
 
-import microservices.comment.apiResponse.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
-import microservices.comment.apiResponse.ApiResponseBuilder;
 import microservices.comment.models.Comment;
 import microservices.comment.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @RestController
@@ -26,119 +24,33 @@ public class CommentController {
     }
 
     @GetMapping("/fetchAll")
-    public ResponseEntity<ApiResponse<List<Comment>>> fetchAll() {
-        ApiResponse<List<Comment>> apiResponse = commentService.fetchAll();
-
-        switch (apiResponse) {
-            case ApiResponse.Success<List<Comment>> success -> {
-                if (success.value().isPresent()) {
-                    log.info("Fetched all comments successfully. Total comments: {}", success.value().get().size());
-                } else {
-                    log.info("Fetched all comments successfully, but no comments found.");
-                }
-                return ResponseEntity.status(HttpStatus.OK).body(success);
-            }
-            case ApiResponse.Failure<List<Comment>> failure -> {
-                log.error("Failed to fetch all comments. Error: {}", failure.errorMessage());
-                return new ResponseEntity<>(failure, failure.status());
-            }
-        }
+    public ResponseEntity<Optional<List<Comment>>> fetchAll() {
+        return commentService.fetchAll();
     }
 
     @GetMapping("/fetchCommentById/{id}")
-    public ResponseEntity<ApiResponse<Comment>> fetchCommentById(@PathVariable long id) {
-        ApiResponse<Comment> apiResponse = commentService.fetchById(id);
-
-        switch (apiResponse) {
-            case ApiResponse.Success<Comment> success -> {
-                if (success.value().isPresent()) {
-                    log.info("Fetched comment with id: {}", success.value().get().getId());
-                } else {
-                    log.info("Fetched comment successfully, but no comment data found.");
-                }
-                return ResponseEntity.status(HttpStatus.OK).body(success);
-            }
-            case ApiResponse.Failure<Comment> failure -> {
-                log.error("Failed to fetch comment with id: {}. Error: {}", id, failure.errorMessage());
-                return new ResponseEntity<>(failure, failure.status());
-            }
-        }
+    public ResponseEntity<Optional<Comment>> fetchCommentById(@PathVariable long id) {
+        return commentService.fetchById(id);
     }
 
     @PostMapping("/saveOneComment")
-    public ResponseEntity<ApiResponse<Comment>> saveOneComment(@RequestBody Comment comment) {
-        ApiResponse<Comment> apiResponse = commentService.saveOneComment(comment);
-
-        switch (apiResponse) {
-            case ApiResponse.Success<Comment> success -> {
-                if (success.value().isPresent()) {
-                    log.info("Saved comment with id: {}", success.value().get().getId());
-                } else {
-                    log.info("Comment saved successfully.");
-                }
-                return ResponseEntity.status(HttpStatus.CREATED).body(success);
-            }
-            case ApiResponse.Failure<Comment> failure -> {
-                log.error("Failed to save comment. Error: {}", failure.errorMessage());
-                return new ResponseEntity<>(failure, failure.status());
-            }
-        }
+    public ResponseEntity<Optional<Comment>> saveOneComment(@RequestBody Comment comment) {
+        return commentService.saveOneComment(comment);
     }
 
     @GetMapping("/user/{userId}/book/{bookId}")
-    public ResponseEntity<ApiResponse<List<Comment>>> fetchCommentsByUserAndBook(@PathVariable Long userId, @PathVariable Long bookId) {
-        ApiResponse<List<Comment>> apiResponse = commentService.fetchCommentsByUserAndBook(userId, bookId);
-
-        switch (apiResponse) {
-            case ApiResponse.Success<List<Comment>> success -> {
-                if (success.value().isPresent()) {
-                    log.info("Fetched comments for userId: {} and bookId: {}. Total comments: {}", userId, bookId, success.value().get().size());
-                } else {
-                    log.info("Fetched comments for userId: {} and bookId: {}, but no comments found.", userId, bookId);
-                }
-                return ResponseEntity.status(HttpStatus.OK).body(success);
-            }
-            case ApiResponse.Failure<List<Comment>> failure -> {
-                log.error("Failed to fetch comments for userId: {} and bookId: {}. Error: {}", userId, bookId, failure.errorMessage());
-                return new ResponseEntity<>(failure, failure.status());
-            }
-        }
+    public ResponseEntity<Optional<List<Comment>>> fetchCommentsByUserAndBook(@PathVariable Long userId, @PathVariable Long bookId) {
+        return commentService.fetchCommentsByUserAndBook(userId, bookId);
     }
 
 
     @PutMapping("/updateComment/user/{userId}/book/{bookId}/comment/{commentId}")
-    public ResponseEntity<ApiResponse<Comment>> updateComment(@PathVariable Long userId, @PathVariable Long bookId, @PathVariable Long commentId, @RequestBody Comment updateComment) {
-        ApiResponse<Comment> apiResponse = commentService.updateComment(userId, bookId, commentId, updateComment);
-  
-        switch (apiResponse) {
-            case ApiResponse.Success<Comment> success -> {
-                if (success.value().isPresent()) {
-                    log.info("Updated comment with id: {}", success.value().get().getId());
-                } else {
-                    log.info("Comment updated successfully.");
-                }
-                return ResponseEntity.status(HttpStatus.OK).body(success);
-            }
-            case ApiResponse.Failure<Comment> failure -> {
-                log.error("Failed to update comment with commentId: {}. Error: {}", commentId, failure.errorMessage());
-                return new ResponseEntity<>(failure, failure.status());
-            }
-        }
+    public ResponseEntity<Optional<Comment>> updateComment(@PathVariable Long userId, @PathVariable Long bookId, @PathVariable Long commentId, @RequestBody Comment updateComment) {
+        return commentService.updateComment(userId, bookId, commentId, updateComment);
     }
 
     @DeleteMapping("/delete/user/{userId}/book/{bookId}/comment/{commentId}")
-    public ResponseEntity<ApiResponse<Void>> deleteComment(@PathVariable Long userId, @PathVariable Long bookId, @PathVariable Long commentId) {
-        ApiResponse<Void> apiResponse = commentService.deleteCommentById(userId, bookId, commentId);
-
-        switch (apiResponse) {
-            case ApiResponse.Success<Void> success -> {
-                log.info("Successfully deleted comment with id: {}", commentId);
-                return ResponseEntity.status(HttpStatus.OK).body(success);
-            }
-            case ApiResponse.Failure<Void> failure -> {
-                log.error("Failed to delete comment with id: {}. Error: {}", commentId, failure.errorMessage());
-                return new ResponseEntity<>(failure, failure.status());
-            }
-        }
+    public ResponseEntity<Optional<Comment>> deleteComment(@PathVariable Long userId, @PathVariable Long bookId, @PathVariable Long commentId) {
+        return commentService.deleteCommentById(userId, bookId, commentId);
     }
 }
