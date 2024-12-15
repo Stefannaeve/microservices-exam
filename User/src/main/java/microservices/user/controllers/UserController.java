4efd3ef82f22f09167107551a3025af2886853ac
Henrik-Pham <HenrikPHPham@gmail.com>
2024-12-15 10:51:24 +0100

    Adding delete methods to userclient

--- User/src/main/java/microservices/user/controllers/UserController.java
+++ User/src/main/java/microservices/user/controllers/UserController.java
@@ -54,16 +54,16 @@ public class UserController {
         return userService.fetchNotFinishedBooks(userId);
     }
 
+    @DeleteMapping("/{userId}/deleteBook/{bookId}")
+    public ResponseEntity<Optional<User>> deleteBookFromUser(@PathVariable Long userId, @PathVariable Long bookId) {
+        return userService.deleteBookFromUser(userId, bookId);
+    }
+
     //
 
     @DeleteMapping("/delete/{userId}")
     public ResponseEntity<Optional<User>> deleteUser(@PathVariable Long userId) {
         return userService.deleteUserById(userId);
-    }
-
-    @DeleteMapping("/{userId}/deleteBook/{bookId}")
-    public ResponseEntity<Optional<User>> deleteBookFromUser(@PathVariable Long userId, @PathVariable Long bookId) {
-        return userService.deleteBookFromUser(userId, bookId);
     }
 
     @PatchMapping("/{userId}/books/{bookId}/progress")
