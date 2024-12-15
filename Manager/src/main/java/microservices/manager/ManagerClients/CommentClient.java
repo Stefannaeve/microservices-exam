94a24e6ae05e2e51da3815b7ed036fcd83af64d8
Henrik-Pham <HenrikPHPham@gmail.com>
2024-12-15 19:32:46 +0100

    adding comment

--- Manager/src/main/java/microservices/manager/ManagerClients/CommentClient.java
+++ Manager/src/main/java/microservices/manager/ManagerClients/CommentClient.java
@@ -20,6 +20,7 @@ public class CommentClient {
     private final String restServiceUrl;
     private final RestTemplate restTemplate;
 
+    //region GET
     public CommentClient(
             RestTemplateBuilder restTemplateBuilder,
             @Value("http://gateway:8000") final String url
