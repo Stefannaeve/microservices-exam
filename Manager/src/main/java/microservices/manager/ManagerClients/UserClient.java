f9a0af64e346595d39a8d785bba014e9f1bde4ea
Henrik-Pham <HenrikPHPham@gmail.com>
2024-12-15 10:17:19 +0100

    Adding jsonproperty to userDTO

--- Manager/src/main/java/microservices/manager/ManagerClients/UserClient.java
+++ Manager/src/main/java/microservices/manager/ManagerClients/UserClient.java
@@ -1,19 +1,17 @@
 package microservices.manager.ManagerClients;
 
 import lombok.extern.slf4j.Slf4j;
-import microservices.manager.responseEntityInitializer.ResponseEntityInitializer;
+import microservices.manager.apiResponse.ResponseEntityInitializer;
 import microservices.manager.dtos.UserDTO;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.View;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 @Slf4j
@@ -22,14 +20,17 @@ public class UserClient {
 
     private final String restServiceUrl;
     private final RestTemplate restTemplate;
+    private final View error;
 
     public UserClient(RestTemplateBuilder restTemplateBuilder,
-                      @Value("http://gateway:8000/user") final String url) {
+                      @Value("http://user:8083/user") final String url,
+                      View error) {
         this.restServiceUrl = url;
-        this.restTemplate = restTemplateBuilder
-                .requestFactory(HttpComponentsClientHttpRequestFactory.class)
-                .build();
+        this.restTemplate = restTemplateBuilder.build();
+        this.error = error;
     }
+
+
 
     public ResponseEntity<Optional<UserDTO>> externalGetUserWithBook(long userId, long bookId) {
         String url = restServiceUrl + "/fetchUserWithBook/" + userId + "/" + bookId;
@@ -76,7 +77,6 @@ public class UserClient {
 
         return response;
     }
-
 
     public ResponseEntity<Optional<UserDTO>> externalSaveOneUser(UserDTO user){
         String url = restServiceUrl + "/saveOneUser";
@@ -307,153 +307,6 @@ public class UserClient {
                     Optional.empty()
             );
         }
-        return response;
-    }
-
-    public ResponseEntity<Optional<UserDTO>> externalDeleteBookFromUser(Long userId, Long bookId) {
-        String url = restServiceUrl + "/" + userId + "/deleteBook/" + bookId;
-        log.error(url);
-        ResponseEntity<Optional<UserDTO>> response = null;
-
-        try {
-            response = restTemplate.exchange(
-                    url,
-                    HttpMethod.DELETE,
-                    null,
-                    new ParameterizedTypeReference<>() {
-                    }
-            );
-        } catch (Exception exception) {
-            log.error("An unexpected error occurred: ", exception);
-        }
-
-        if (response == null){
-            return ResponseEntityInitializer.NewResponseEntity(
-                    HttpStatus.NO_CONTENT,
-                    false,
-                    "Response empty from the book service",
-                    HttpStatus.NO_CONTENT,
-                    Optional.empty());
-        }
-
-        String success = ResponseEntityInitializer.extractHeader(response, "success");
-        log.info("Success: {}", success);
-
-        if (success.equals("false")){
-            String errorMessage = ResponseEntityInitializer.extractHeader(response, "ErrorMessage");
-            String errorStatus = ResponseEntityInitializer.extractHeader(response, "ErrorStatus");
-            HttpStatus status = HttpStatus.valueOf(Integer.parseInt(errorStatus));
-            return ResponseEntityInitializer.NewResponseEntity(
-                    status,
-                    false,
-                    errorMessage,
-                    status,
-                    response.getBody()
-            );
-        }
-        return response;
-    }
-
-    public ResponseEntity<Optional<UserDTO>> externalDeleteUserById(Long userId) {
-        String url = restServiceUrl + "/delete/" + userId;
-        log.error(url);
-        ResponseEntity<Optional<UserDTO>> response = null;
-
-
-        try {
-            response = restTemplate.exchange(
-                    url,
-                    HttpMethod.DELETE,
-                    null,
-                    new ParameterizedTypeReference<>() {
-                    }
-            );
-        } catch (Exception exception) {
-            log.error("An unexpected error occurred: ", exception);
-        }
-
-        if (response == null){
-            return ResponseEntityInitializer.NewResponseEntity(
-                    HttpStatus.NO_CONTENT,
-                    false,
-                    "Response empty from the book service",
-                    HttpStatus.NO_CONTENT,
-                    Optional.empty());
-        }
-
-        String success = ResponseEntityInitializer.extractHeader(response, "success");
-        log.info("Success: {}", success);
-
-        if (success.equals("false")){
-            String errorMessage = ResponseEntityInitializer.extractHeader(response, "ErrorMessage");
-            String errorStatus = ResponseEntityInitializer.extractHeader(response, "ErrorStatus");
-            HttpStatus status = HttpStatus.valueOf(Integer.parseInt(errorStatus));
-            return ResponseEntityInitializer.NewResponseEntity(
-                    status,
-                    false,
-                    errorMessage,
-                    status,
-                    response.getBody()
-            );
-        }
-        return response;
-    }
-
-    public ResponseEntity<Optional<UserDTO>> externalUpdateReadingProgress(Long userId, Long bookId, Map<String, String> requestBody) {
-        String url = restServiceUrl + "/" + userId + "/books/" + bookId + "/progress";
-        log.info("Request URL: {}", url);
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<Map<String, String>> requestEntity = new HttpEntity<>(requestBody, headers);
-
-        ResponseEntity<Optional<UserDTO>> response = null;
-
-        try {
-            response = restTemplate.exchange(
-                    url,
-                    HttpMethod.PATCH,
-                    requestEntity,
-                    new ParameterizedTypeReference<>() {}
-            );
-        } catch (Exception exception) {
-            log.error("An unexpected error occurred: {}", exception.getMessage());
-            return ResponseEntityInitializer.NewResponseEntity(
-                    HttpStatus.INTERNAL_SERVER_ERROR,
-                    false,
-                    "An error occurred while updating reading progress",
-                    HttpStatus.INTERNAL_SERVER_ERROR,
-                    Optional.empty()
-            );
-        }
-
-        if (response == null) {
-            return ResponseEntityInitializer.NewResponseEntity(
-                    HttpStatus.NO_CONTENT,
-                    false,
-                    "Response empty from the user service",
-                    HttpStatus.NO_CONTENT,
-                    Optional.empty()
-            );
-        }
-
-        String success = ResponseEntityInitializer.extractHeader(response, "success");
-        log.info("Success: {}", success);
-
-        if ("false".equals(success)) {
-            String errorMessage = ResponseEntityInitializer.extractHeader(response, "ErrorMessage");
-            String errorStatus = ResponseEntityInitializer.extractHeader(response, "ErrorStatus");
-            HttpStatus status = HttpStatus.valueOf(Integer.parseInt(errorStatus));
-
-            return ResponseEntityInitializer.NewResponseEntity(
-                    status,
-                    false,
-                    errorMessage,
-                    status,
-                    response.getBody()
-            );
-        }
-
         return response;
     }
 }
